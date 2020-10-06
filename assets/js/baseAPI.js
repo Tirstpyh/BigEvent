2ad686250212aa329d66881698fa4df56c63e905
pyh <1196316326@qq.com>
2020-10-06 21:15:28 +0800

    完成了主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,25 @@
 // jquery提供的ajaxPrefilter函数在每次发起请求前都会自动调用
 //可以在这时候拼接上根路径,便于管理
-$.ajaxPrefilter(function(options) {
-    //请求根路径
-    const URL = 'http://ajax.frontend.itheima.net'
-    options.url = URL + options.url
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+    // 为有权限的接口，设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: sessionStorage.getItem('token') || ''
+        }
+    }
+
+    //优化complate
+    options.complete = function(res) {
+        //使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+          layer.msg('钥匙都不拿就想进门,不存在的!');
+          //  强制清空 token
+          sessionStorage.removeItem('token')
+          //  强制跳转到登录页面
+          location.href = '/login.html'
+          
+        }
+      }
 })
